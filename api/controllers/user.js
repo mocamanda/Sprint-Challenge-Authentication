613bc1abd24198bb184f72d83f90416fb75a4179
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-18 10:53:16 -0700

    Creating User via user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
+  const newUser = new User ({ username, password });
+  newUser.save((err, user) => {
+    if (err) return res.send(err);
+    res.json({
+      success: 'User Saved',
+      user
+    });
+  });
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
 };
